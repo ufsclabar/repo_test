affa8f00f6df9c1bd308d63f49eec4bf53cc51ea
ufsclabar <89537623+ufsclabar@users.noreply.github.com>
2021-08-25 13:30:24 -0300

    adicionando o script teste

---
+++ script_teste.R
@@ -0,0 +1,15 @@
+#################################################################################
+## Prática Github - Curso Introdução ao Controle de Versão com Git/Github/RStudio 
+## 27 de agosto - 2021
+##
+#################################################################################
+
+# Instruções
+# Para concluir a tarefa `fork`, `branch`, `pull request` e `merge`,
+# Basta você modificar este Script. A modificação que solicito é que você
+# coloque seu nome completo na lista abaixo.
+
+# Lista de Nomes:
+# 1. Thiago Cesar Lima Silveira
+# 2.
+# 3.
